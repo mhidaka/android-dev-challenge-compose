de1ff137243ee21be5ff58adbf778a4645520254
mhidaka <rabbitlog@gmail.com>
2021-02-27 17:41:30 +0900

    Implements fake repostory / ねこさまリスト取得の実装

--- app/src/main/java/com/example/androiddevchallenge/repository/FakeAdaptionRepository.kt
+++ app/src/main/java/com/example/androiddevchallenge/repository/FakeAdaptionRepository.kt
@@ -1,13 +1,21 @@
 package com.example.androiddevchallenge.repository
 
 import com.example.androiddevchallenge.content.Cat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class FakeAdaptionRepository : Repository{
-    override suspend fun getPost(postId: String): Result<Cat> {
-        TODO("Not yet implemented")
+class FakeAdaptionRepository : Repository {
+
+    override suspend fun getPost(postId: String): Result<Cat> = withContext(Dispatchers.IO) {
+        val post = posts.find { it.id == postId }
+        if (post == null) {
+            Result.Error(IllegalArgumentException("Unable to find post"))
+        } else {
+            Result.Success(post)
+        }
     }
 
     override suspend fun getPosts(): Result<List<Cat>> {
-        TODO("Not yet implemented")
+        return Result.Success(posts)
     }
 }
