4dcbc5cc979e812311223288d9b5f11754660128
mhidaka <rabbitlog@gmail.com>
2021-02-27 21:24:05 +0900

    Add Featured post view for most variable cat / スゴいねこちゃんをフィーチャーする機能を追加（ずきゅんときた場合に使う）

---
+++ app/src/main/java/com/example/androiddevchallenge/ui/FeaturedItem.kt
@@ -0,0 +1,105 @@
+package com.example.androiddevchallenge.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.ThemedPreview
+import com.example.androiddevchallenge.content.Cat
+import com.example.androiddevchallenge.content.Contributor
+import com.example.androiddevchallenge.repository.post1
+import com.example.androiddevchallenge.repository.post6
+
+@Composable
+fun FeaturedItem(cat: Cat, modifier: Modifier = Modifier) {
+    val typography = MaterialTheme.typography
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        val imageModifier = Modifier
+            .heightIn(min = 180.dp, max = 180.dp)
+            .fillMaxWidth()
+            .clip(shape = MaterialTheme.shapes.medium)
+        Image(
+            painter = painterResource(cat.imageId),
+            contentDescription = null, // decorative
+            modifier = imageModifier,
+            contentScale = ContentScale.Crop
+        )
+        Spacer(Modifier.height(16.dp))
+
+        Text(
+            text = cat.title,
+            style = typography.h6
+        )
+
+        Spacer(Modifier.height(16.dp))
+
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Row() {
+                Image(
+                    painter = painterResource(cat.contributor.imageId),
+                    contentDescription = null, // decorative
+                    modifier = modifier
+                        .padding(end = 16.dp)
+                        .size(45.dp, 45.dp)
+                        .clip(RoundedCornerShape(percent = 50))
+                )
+                CntributorsComment(cat.contributor)
+            }
+        }
+    }
+}
+
+@Composable
+fun CntributorsComment(contributor: Contributor) {
+    val typography = MaterialTheme.typography
+    Column() {
+        Text(
+            text = contributor.shortIntroduction,
+            style = typography.body2
+        )
+        Text(
+            text = "${contributor.name} - 2 minutes left",
+            style = typography.body2
+        )
+    }
+}
+
+
+@Preview("Featured Item")
+@Composable
+fun FeaturedItemPreview() {
+    ThemedPreview {
+        FeaturedItem(cat = post1)
+    }
+}
+
+
+@Preview("Featured Item Dark theme")
+@Composable
+fun FeaturedItemDarkThemePreview() {
+    ThemedPreview(darkTheme = true) {
+        FeaturedItem(cat = post6)
+    }
+}
